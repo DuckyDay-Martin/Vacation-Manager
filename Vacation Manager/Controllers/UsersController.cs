b7f44e726cfa43d560f905837c9af007d4f53f4f
Мартин Кондакчиев <mk88923724@edu.mon.bg>
2023-03-24 17:41:24 +0200

    Add project files.

---
+++ Vacation Manager/Controllers/UsersController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.Entity;
+
+namespace Vacation_Manager.Controllers
+{
+    public class UsersController : Controller
+    {
+       
+    }
+}
---
+++ Vacation Manager/Models/UserModels.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vacation_Manager.Models
+{
+    public class UserModels
+    {
+        int UserId { get; set; }
+
+        [Required]
+        string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+        //TODO:...
+
+        //Default конструктор 
+        public UserModels()
+        {
+            UserId = 0;
+            UserName = " ";
+            Password= " ";
+        }
+
+        public UserModels(int userId, string userName, string password)
+        {
+            UserId = userId;
+            UserName = userName;
+            Password = password;
+        }
+    }
+}
